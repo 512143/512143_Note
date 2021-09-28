42b28b91f9d91ef66a63f135169c807efeb1b304
512143 <dan.kedrov@yandex.ru>
2021-09-28 19:21:17 +0300

    note.V 2

---
+++ 512143_Note/512143_Note/Contexts/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _512143_Note.Models;
+
+namespace _512143_Note.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext (DbContextOptions<NoteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Notes> Notes { get; set; }
+    }
+}
---
+++ 512143_Note/512143_Note/Models/Notes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _512143_Note.Models
+{
+    public class Notes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set;  }
+        public DateTime? CreateDate { get; set; }
+
+    }
+}
